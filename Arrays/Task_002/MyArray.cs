5a0d883bd86f404dc9942eac7ab0c587aeab5860
Ghoust48 <slepuhin.ghoust48@gmail.com>
2020-03-08 23:19:02 +0300

    Second task completed

---
+++ Arrays/Task_002/MyArray.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_002
+{
+    public class MyArray
+    {
+        private int[] _array;
+        
+        public MyArray(int length)
+        {
+            _array = new int[length];
+        }
+        
+        public void Fill()
+        {
+            var rand = new Random();
+            
+            for (var i = 0; i < _array.Length; i++)
+            {
+                _array[i] = rand.Next(-10, 10);
+            }
+        }
+
+        public void Show()
+        {
+            for (var i = 0; i < _array.Length; i++)
+            {
+                Console.WriteLine(_array[i]);
+            }
+
+            Console.WriteLine(new string('-', 20));
+        }
+
+        public void Max()
+        {
+            var max = 0;
+            
+            for (var i = 0; i < _array.Length; i++)
+            {
+                if (max < _array[i])
+                {
+                    max = _array[i];
+                }
+            }
+
+            Console.WriteLine(max);
+            Console.WriteLine(new string('-', 20));
+        }
+
+        public void Min()
+        {
+            var min = 0;
+
+            for (var i = 0; i < _array.Length; i++)
+            {
+                if (min > _array[i])
+                {
+                    min = _array[i];
+                }
+            }
+
+            Console.WriteLine(min);
+            Console.WriteLine(new string('-', 20));
+        }
+
+        public void Sum()
+        {
+            var sum = 0;
+            
+            for (var i = 0; i < _array.Length; i++)
+            {
+                sum += _array[i];
+            }
+
+            Console.WriteLine(sum);
+            Console.WriteLine(new string('-', 20));
+        }
+
+        public void Average()
+        {
+            var sum = 0;
+            
+            for (var i = 0; i < _array.Length; i++)
+            {
+                sum += _array[i];
+            }
+            
+            Console.WriteLine(sum / _array.Length);
+            Console.WriteLine(new string('-', 20));
+        }
+
+        public void Odd()
+        {
+            for (var i = 0; i < _array.Length; i++)
+            {
+                if (_array[i] % 2 != 0)
+                {
+                    Console.WriteLine(_array[i]);
+                }
+            }
+            Console.WriteLine(new string('-', 20));
+        }
+    }
+}
---
+++ Arrays/Task_002/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите размер массива: ");
+            var length = int.Parse(Console.ReadLine());
+
+            var arr = new MyArray(length);
+
+            arr.Fill();
+            arr.Show();
+            arr.Max();
+            arr.Min();
+            arr.Sum();
+            arr.Average();
+            arr.Odd();
+
+            Console.ReadLine();
+        }
+
+        
+    }
+}
